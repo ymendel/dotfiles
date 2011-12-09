a2f8124ace5875fcc433d1fdc83e07d15d0e9843
Yossef Mendelssohn <ymendel@pobox.com>
2011-12-09 03:40:03 -0500

    Making a tiny .bashrc and pulling in nested files
    Now the fun organization necessary setup is pretty much done. With a
    tiny .bash_profile (that just pulls in .bashrc) and a tiny .bashrc (that
    pulls in all the nested stuff + the special .local file), the rest is
    just the actual organization work. And the old .bashrc is now pulled out
    into its own setup file.

---
+++ shell/setup.bash
@@ -0,0 +1,21 @@
+export PATH="$PATH:~/scripts"
+
+export PS1="\h:\w\$(git_display) \u\$ "
+export PROMPT_COMMAND='echo -ne "\033]0;${USER}@${HOSTNAME%%.*}\007"'
+
+export EDITOR=vi
+
+export LC_CTYPE=en_us.UTF-8
+
+shopt -s histappend
+export HISTSIZE=10000
+export HISTFILESIZE=${HISTSIZE}
+export HISTCONTROL="ignoredups"
+export HISTIGNORE="&:l[salh]:[bf]g:exit"
+
+for f in aliases functions bash_completion_base
+do
+  test -r ~/.$f && source ~/.$f
+done
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
