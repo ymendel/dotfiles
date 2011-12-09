74861bce44239a35c5814948f071ae46536e364f
Yossef Mendelssohn <ymendel@pobox.com>
2011-12-09 03:55:40 -0500

    Adding git directory
    Moving gitconfig to place in git directory.
    Moving git-related aliases to git directory.

---
+++ git/aliases.bash
@@ -0,0 +1,14 @@
+# gitness
+alias h=hack
+alias s=ship
+alias shipm="ship -m"
+alias sm=shipm
+
+# githubness
+alias ghr=github_repo_create.rb
+
+# gistness
+alias pg="pbpaste | gist --open"
+alias ppg="pbpaste | gist --private --open"
+alias rpg="pg -t .rb"
+alias rppg="ppg -t .rb"
