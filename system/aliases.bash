f834e1ed77f8def74c9d252770c16bff1b424799
Yossef Mendelssohn <ymendel@pobox.com>
2011-12-09 21:18:44 -0500

    Moving things to the system dir
    new_tab and new_term go in system/bin
    A bunch of aliases go as well, things that are obviously system-related.
    I am not at all conflicted with these, unlike my questioning of the
    'grok' and 'wiki' functions.

---
+++ system/aliases.bash
@@ -0,0 +1,22 @@
+# some commands need defaults
+alias ls="ls -qF"
+alias bc="bc -ql"
+alias mvim="mvim -p"
+
+# ls shorthand
+alias la="ls -a"
+alias ll="ls -l"
+alias lh="ls -lh"
+alias lal="ls -al"
+
+# new tabs and terms
+alias tab=new_tab
+alias new=new_term
+
+# finding processes
+alias find_process="ps auxwwww | grep -v grep | grep "
+alias find_ps=find_process
+alias fps=find_process
+
+alias where="type -ap"
+alias so=sudo
