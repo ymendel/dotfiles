96d8fc4b445d1144390fba817f9af9fff6102a20
Yossef Mendelssohn <ymendel@pobox.com>
2011-12-09 22:16:00 -0500

    Adding work directory
    punch is obviously for work

---
+++ work/aliases.bash
@@ -0,0 +1,13 @@
+# punch stuff
+alias punch_out_all="punch out"
+alias pi="punch in"
+alias po="punch out"
+alias pout="punch out"
+alias poa=punch_out_all
+alias pl="punch list"
+alias pt="punch total"
+alias pstat="punch status"
+alias pst="punch status"
+alias psts="punch status --short"
+alias pstsf="punch status --short --full"
+alias psum="punch summary"
