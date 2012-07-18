087aea0c0257c1bfb2c8f946b1077e6300d80c27
Yossef Mendelssohn <ymendel@pobox.com>
2012-07-18 12:06:42 -0400

    Add mvim_directory_server arguments for git
    I like the convenience of 'v', but I dislike when I'm in some sort of
    project that have to change directory to get a file to open in the right
    instance. This lets me use 'vg' to use the git root as the instance
    name.
    
    If this becomes a problem outside of git repos, I'll figure out what
    to do then.

--- system/editing.bash
+++ system/editing.bash
@@ -5,9 +5,22 @@ mvim_directory_server()
   if [ $# == 0 ]
   then
     mvim
+  elif [ ${1:0:2} = '--' ]
+  then
+      if [ $1 = '--git-root' ]
+      then
+        mvim --servername $(basename $(git top)) --remote-tab-silent "${@:2}" 1>/dev/null 2>&1
+      elif [ $1 = '--pwd' ]
+      then
+        mvim --servername $(basename $(pwd)) --remote-tab-silent "${@:2}" 1>/dev/null 2>&1
+      else
+          echo 'what does that mean?'
+      fi
   else
     mvim --servername $(basename $(pwd)) --remote-tab-silent "$@" 1>/dev/null 2>&1
   fi
 }
 
 alias v=mvim_directory_server
+alias vg="mvim_directory_server --git-root"
+alias vp="mvim_directory_server --pwd"
