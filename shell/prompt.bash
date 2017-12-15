caee66daa638a81cd9c9a0ec9622084600a46c4f
Yossef Mendelssohn <ymendel@pobox.com>
2017-12-15 11:12:13 -0500

    add git ahead/behind to shell prompt
    Just a little bit more info for my shell prompt.
    
    Also making some sort of convenience, with a function to detect whether
    I'm in a git repo. And putting a tiny bit of space in the prompt (before
    the git-display, so the path and current-head don't run together.
    
    I may need more space with the ahead/behind. We'll see.
    
    Maybe time for colors soon, too.

--- shell/prompt.bash
+++ shell/prompt.bash
@@ -1,5 +1,10 @@
 export PS1="\h:\w\$(git_display) \u\$ "
 
+in_git_repo()
+{
+  git rev-parse --git-dir > /dev/null 2>&1
+}
+
 git_current_branch()
 {
   git branch 2>/dev/null | awk '/^\* /{print $2}'
@@ -20,10 +25,29 @@ git_dirty()
   (git status --porcelain | grep -qe .) > /dev/null 2>&1 && echo '*'
 }
 
+git_ahead_behind()
+{
+  BRANCH_INFO=`git status --porcelain --branch | head -1`
+  AHEAD_BEHIND=''
+  if [[ $BRANCH_INFO =~ behind\ ([0-9]+) ]]
+  then
+    AHEAD_BEHIND+="↓${BASH_REMATCH[1]}"
+  fi
+  if [[ $BRANCH_INFO =~ ahead\ ([0-9]+) ]]
+  then
+    AHEAD_BEHIND+="↑${BASH_REMATCH[1]}"
+  fi
+  echo $AHEAD_BEHIND
+}
+
 git_display()
 {
-  export GIT_DIRTY=`git_dirty`
-  git_current_head | awk '{if ($1) print "(" $1 ENVIRON["GIT_DIRTY"] ")"}'
+  if in_git_repo
+  then
+    export GIT_DIRTY=`git_dirty`
+    export GIT_AHEAD_BEHIND=`git_ahead_behind`
+    git_current_head | awk '{if ($1) print " (" $1 ENVIRON["GIT_DIRTY"] ENVIRON["GIT_AHEAD_BEHIND"] ")"}'
+  fi
 }
 
 rvm_current()
