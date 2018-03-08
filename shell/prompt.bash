184fd600a56e045e85e7a311d8d6a9bb19277692
Yossef Mendelssohn <ymendel@pobox.com>
2018-03-08 15:11:00 -0500

    get rid of unneeded git prompt functions
    Don't need these anymore, because it happens all in the `set_prompt`
    function. Also haven't needed to know about rvm for a while.
    
    And fixing the indentation to four spaces.

--- shell/prompt.bash
+++ shell/prompt.bash
@@ -30,68 +30,35 @@ set_prompt()
 
 in_git_repo()
 {
-  git rev-parse --git-dir > /dev/null 2>&1
+    git rev-parse --git-dir > /dev/null 2>&1
 }
 
 git_current_branch()
 {
-  git branch 2>/dev/null | awk '/^\* /{print $2}'
+    git branch 2>/dev/null | awk '/^\* /{print $2}'
 }
 
 git_current_head()
 {
-  BRANCH=`git_current_branch`
-  if [[ $BRANCH = "(detached" ]]
-  then
-    BRANCH=`git name-rev --name-only HEAD 2>/dev/null`
-  fi
-  echo $BRANCH
+    BRANCH=`git_current_branch`
+    if [[ $BRANCH = "(detached" ]]
+    then
+      BRANCH=`git name-rev --name-only HEAD 2>/dev/null`
+    fi
+    echo $BRANCH
 }
 
 git_current_rev()
 {
-  git rev-parse --short=4 HEAD
+    git rev-parse --short=4 HEAD
 }
 
 git_dirty()
 {
-  (git status --porcelain | grep -qe .) > /dev/null 2>&1 && echo -n '*'
+    (git status --porcelain | grep -qe .) > /dev/null 2>&1 && echo -n '*'
 }
 
 git_branch_info()
 {
-  git status --porcelain --branch | head -1
-}
-
-git_ahead_behind()
-{
-  AHEAD_BEHIND=''
-  if [[ $BRANCH_INFO =~ behind\ ([0-9]+) ]]
-  then
-    AHEAD_BEHIND+=" ↓${BASH_REMATCH[1]}"
-  fi
-  if [[ $BRANCH_INFO =~ ahead\ ([0-9]+) ]]
-  then
-    AHEAD_BEHIND+=" ↑${BASH_REMATCH[1]}"
-  fi
-
-  echo $AHEAD_BEHIND
-}
-
-git_display()
-{
-  if in_git_repo
-  then
-    GIT_AHEAD_BEHIND=`git_ahead_behind`
-    if [[ $GIT_AHEAD_BEHIND != '' ]]
-    then
-        GIT_AHEAD_BEHIND=" ${GIT_AHEAD_BEHIND}"
-    fi
-    echo " ($(git_current_head)@$(git_current_rev)$(git_dirty)${GIT_AHEAD_BEHIND})"
-  fi
-}
-
-rvm_current()
-{
-  rvm info | sed -n '2,1s/:$//p'
+    git status --porcelain --branch | head -1
 }
