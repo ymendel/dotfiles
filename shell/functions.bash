2aee22a49b36749e952146492da22b81788d62ae
Yossef Mendelssohn <ymendel@pobox.com>
2018-03-08 14:57:20 -0500

    take out echoc function
    Don't need this anymore, because the variables are simple. Maybe it's
    something I might bring back later, but seems unlikely.
    
    Also using the named variables for the divider function. That's more
    readable.

--- shell/functions.bash
+++ shell/functions.bash
@@ -1,14 +1,10 @@
-echoc() {
-  echo -e "${1}$2${ResetColor}" | sed 's/\\\]//g'  | sed 's/\\\[//g'
-}
-
 # inspired by http://beckism.com/2009/02/better_bash_prompt/#comment-19411
 divider()
 {
-    echo -ne "\033[1;38;43m"
+    echo -n "${Bold}${YellowBg}"
     for ((x=1; x <= $COLUMNS; x++))
     do
-        echo -ne "="
+        echo -n "="
     done
-    echo -e  "\033[0m"
+    echo $ResetColor
 }
