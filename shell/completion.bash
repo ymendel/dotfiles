64fdb29c60a34f6ad290d632d4f7d0a97380022b
Yossef Mendelssohn <ymendel@pobox.com>
2021-04-29 13:32:46 -0400

    getting bash completion on new machine
    Apparently /usr/local is for Intel and /opt/homebrew is for ARM, for
    whatever reasons. So here we go.
    
    Unfortunately, this tries to do something with brew before
    /opt/homebrew/bin is added to the path. I've gotten around that with a
    symlink in /usr/local/bin. YMMV

--- shell/completion.bash
+++ shell/completion.bash
@@ -1,3 +1,6 @@
 if [ -f /usr/local/etc/bash_completion ]; then
   . /usr/local/etc/bash_completion
 fi
+if [ -f /opt/homebrew/etc/profile.d/bash_completion.sh ]; then
+  . /opt/homebrew/etc/profile.d/bash_completion.sh
+fi
