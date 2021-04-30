37a85f0451396881063764c744c22f1a102d09fc
Yossef Mendelssohn <ymendel@pobox.com>
2021-04-30 13:57:18 -0400

    add /opt/hombrew to path before completion
    The symlink in /usr/local caused more trouble than it helped, so just
    putting a different hack on this, for now.

--- shell/completion.bash
+++ shell/completion.bash
@@ -1,6 +1,7 @@
 if [ -f /usr/local/etc/bash_completion ]; then
   . /usr/local/etc/bash_completion
 fi
+export PATH="/opt/homebrew/bin:$PATH"
 if [ -f /opt/homebrew/etc/profile.d/bash_completion.sh ]; then
   . /opt/homebrew/etc/profile.d/bash_completion.sh
 fi
