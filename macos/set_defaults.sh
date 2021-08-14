a427a9e26a67bf60e44acfb1d124c32b7d9f9398
Yossef Mendelssohn <ymendel@pobox.com>
2021-08-13 21:39:59 -0400

    getting a few more finder defaults in
    Just taking some time to mess with this more, looking at my old defaults
    and what's changed out there in the ecosystem in the meantime.

--- macos/set_defaults.sh
+++ macos/set_defaults.sh
@@ -5,4 +5,12 @@ then
     exit 0
 fi
 
+# cf. https://mths.be/macos
+
+# Close any open System Preferences panes, to prevent them from overriding
+# settings we’re about to change
+# osascript -e 'tell application "System Preferences" to quit'
+# Mathias does it with that osascript, but why not killall instead?
+killall "System Preferences" 2>/dev/null
+
 find $(dirname $0) -name *.defaults | while read defaults; do source ${defaults} ; done
