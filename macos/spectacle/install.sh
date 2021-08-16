e84821410a85e01fdef853090224343f50677337
Yossef Mendelssohn <ymendel@pobox.com>
2021-08-16 09:36:30 -0400

    fix read-within-read for script/install
    No need to pipe find output to read in order to run things. That's what
    -exec is for.

--- macos/spectacle/install.sh
+++ macos/spectacle/install.sh
@@ -5,9 +5,7 @@ killall "Spectacle" &>/dev/null
 source "${DOTFILES_HOME}/script/helpers/printing.sh"
 source "${DOTFILES_HOME}/script/helpers/linking.sh"
 
-# FIXME: read-within-read makes it so the prompt for choice doesn't actually take user input
-# so once that works, this should go back to backup_all=false
-overwrite_all=false backup_all=true skip_all=false
+overwrite_all=false backup_all=false skip_all=false
 
 # TODO: get the full path of the present dir, without specifying all of this
 link_file "$DOTFILES_HOME/macos/spectacle/Shortcuts.json" "$HOME/Library/Application Support/Spectacle/Shortcuts.json"
