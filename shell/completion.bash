e1b64abb64ece36e801d088de96c14a01c09ba1b
Yossef Mendelssohn <ymendel@pobox.com>
2021-08-22 11:24:24 -0400

    updating some bash completion

--- shell/completion.bash
+++ shell/completion.bash
@@ -1,7 +1,1 @@
-if [ -f /usr/local/etc/bash_completion ]; then
-  . /usr/local/etc/bash_completion
-fi
-export PATH="/opt/homebrew/bin:$PATH"
-if [ -f /opt/homebrew/etc/profile.d/bash_completion.sh ]; then
-  . /opt/homebrew/etc/profile.d/bash_completion.sh
-fi
+[[ -r "$(brew --prefix)/etc/profile.d/bash_completion.sh" ]] && . "$(brew --prefix)/etc/profile.d/bash_completion.sh"
