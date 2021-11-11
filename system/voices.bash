9d582f1094244247b18426f38ff1a6bdf2ff7de2
Yossef Mendelssohn <ymendel@pobox.com>
2021-11-11 13:38:31 -0500

    tell which voice is being used by `random_say`

--- system/voices.bash
+++ system/voices.bash
@@ -2,4 +2,4 @@
 alias voices="say -v ?"
 alias voice_names="voices | awk '{print \$1}'"
 alias random_voice="voice_names | sort -R | head -1"
-alias random_say="say -v \`random_voice\`"
+alias random_say="voice=\`random_voice\`; echo \"using \$voice\"; say -v \$voice"
