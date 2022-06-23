7360e004d636828c717b6999a6d46bd02329c54c
Yossef Mendelssohn <ymendel@pobox.com>
2022-06-23 11:19:19 -0400

    updating npm use to deal with deprecation

--- node/functions.bash
+++ node/functions.bash
@@ -1,7 +1,7 @@
 npm_installed () {
-    npm list --global | grep --quiet " ${1}@"
+    npm list --location=global | grep --quiet " ${1}@"
 }
 
 npm_install_with_check () {
-    npm_installed $1 || npm install --global $1
+    npm_installed $1 || npm install --location=global $1
 }
