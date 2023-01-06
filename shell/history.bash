df30738081bbb7a4769648ae3c887a1609e65fa9
Yossef Mendelssohn <ymendel@pobox.com>
2023-01-06 10:09:12 -0500

    adding ignorespace to HISTCONTROL

--- shell/history.bash
+++ shell/history.bash
@@ -1,5 +1,5 @@
 shopt -s histappend
 export HISTSIZE=10000
 export HISTFILESIZE=${HISTSIZE}
-export HISTCONTROL="ignoredups"
+export HISTCONTROL="ignoredups:ignorespace"
 export HISTIGNORE="&:l[salh]:[bf]g:exit"
